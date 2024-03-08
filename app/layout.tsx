40e5ab2abec165606c27f205937ed8227cc6e7e9
Popolitain06 <pomaresguillaume@gmail.com>
2024-03-08 13:31:26 +0100

    Finishing desktop and mobile menu , added mongodb database whith models , added utils and added types

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,13 +1,13 @@
 import type { Metadata } from "next";
-import { IBM_Plex_Sans } from "next/font/google";
+import { Roboto } from "next/font/google";
 import "./globals.css";
 import { cn } from "@/lib/utils";
 import { ClerkProvider } from '@clerk/nextjs'
 
-const IBMPlex = IBM_Plex_Sans({ 
+const roboto = Roboto({ 
   subsets: ["latin"],
-  weight: ["400", "500", "600", "700"],
-  variable: "--font-ibm-plex",
+  weight: ["400", "500", "700"],
+  style: ['normal', 'italic'],
 });
 
 export const metadata: Metadata = {
@@ -26,7 +26,7 @@ export default function RootLayout({
       variables: {colorPrimary: '#624cf5'}}}>
 
       <html lang="en">
-        <body className={cn("font-IBMPlex antialiased ", IBMPlex.variable)}>{children}
+        <body className={cn("font-Roboto ", roboto.className)}>{children}
         </body>
       </html>
 
--- components/shared/MobileNav.tsx
+++ components/shared/MobileNav.tsx
@@ -1,8 +1,94 @@
+'use client'
 import React from 'react'
+import Link from "next/link"
+import Image from 'next/image'
+import {
+  Sheet,
+  SheetContent,
+  SheetTrigger,
+} from "@/components/ui/sheet"
+import { SignedIn, SignedOut } from '@clerk/nextjs'
+import { UserButton } from "@clerk/nextjs";
+import { usePathname } from 'next/navigation'
+import { Button } from '../ui/button'
+import { navLinks } from '@/constants'
+
 
 const MobileNav = () => {
+
+  const pathname = usePathname();
+
   return (
-    <div>MobileNav</div>
+    <header className='header'>
+      <Link href='/' className='flrx items-center gap-2 md:py-2'>
+        <Image
+        src="/logo-text.png" 
+        alt="logo"
+        width={180}
+        height={28}/>
+      </Link>
+
+      <nav className='flex gap-2'>
+
+        <SignedIn>
+          <UserButton afterSignOutUrl='/'/>
+
+            <Sheet>
+              <SheetTrigger>
+                <Image 
+                src="/assets/icons/menu.svg" 
+                alt="menu" 
+                width={32} 
+                height={32}
+                className='cursor-pointer'/>
+
+              </SheetTrigger>
+              <SheetContent className='sheet-content' >
+                <>
+                  <Image src="/logo-text.png" alt='logo' width={152} height={23} />
+
+                  <ul className="header-nav_elements">
+                    {navLinks.slice(0, 6).map((link) => {
+                      const isActive = link.route === pathname
+
+                      return (
+                        <li key={link.route} className={`${isActive && 'gradient-text'} p-18 flex whitespace-nowrap text-dark-700`}>
+                          <Link className="sidebar-link cursor-pointer" href={link.route}>
+                            <Image 
+                              src={link.icon}
+                              alt="logo"
+                              width={24}
+                              height={24}
+                            />
+                            {link.label}
+                          </Link>
+                        </li>
+                      )
+                    })}
+                 </ul>
+                
+                
+                
+                
+                </>
+              </SheetContent>
+            </Sheet>
+
+
+
+        </SignedIn>
+
+        <SignedOut>
+            <Button asChild className="button bg-purple-gradient bg-cover">
+              <Link href="/sign-in">Login</Link>
+            </Button>
+          </SignedOut>
+
+
+
+
+      </nav>
+    </header>
   )
 }
 
